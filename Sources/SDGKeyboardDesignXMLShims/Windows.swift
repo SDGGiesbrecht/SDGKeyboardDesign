c8adef4dc0644fb908f9b9546fda22bc3167198b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-07 13:05:35 -0600

    Fixed Windows build.

---
+++ Sources/SDGKeyboardDesignXMLShims/Windows.swift
@@ -0,0 +1,15 @@
+/*
+ Windows.swift
+
+ This source file is part of the SDGKeyboardDesign open source project.
+ https://sdggiesbrecht.github.io/SDGKeyboardDesign
+
+ Copyright ©2020 Jeremy David Giesbrecht and the SDGKeyboardDesign project contributors.
+
+ Soli Deo gloria.
+ */
+
+#if os(Windows)
+  // #workaround(workspace version 0.32.0, CMake won’t create empty libraries.)
+  public func _cmakePleaseCreateLibrary()
+#endif
