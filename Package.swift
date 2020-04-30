b62a52119a1a53f45f235ca5273b5f0ada47fd85
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-29 19:36:04 -0600

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -267,12 +267,12 @@ let package = Package(
     .library(name: "SDGKeyboardDesign", targets: ["SDGKeyboardDesign"])
   ],
   dependencies: [
-    .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", from: Version(4, 6, 0)),
+    .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", from: Version(5, 0, 0)),
     .package(
       url: "https://github.com/SDGGiesbrecht/SDGInterface",
-      .upToNextMinor(from: Version(0, 7, 0))
+      .upToNextMinor(from: Version(0, 8, 0))
     ),
-    .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", from: Version(5, 2, 0)),
+    .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", from: Version(5, 3, 1)),
   ],
   targets: [
 
