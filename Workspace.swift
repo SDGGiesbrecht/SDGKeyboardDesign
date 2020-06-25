cccbbd1b8e1c78846d45fedca323271978a60bc4
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-24 20:12:49 -0600

    Bumped version.

--- Workspace.swift
+++ Workspace.swift
@@ -14,7 +14,7 @@ import WorkspaceConfiguration
 let configuration = WorkspaceConfiguration()
 configuration._applySDGDefaults()
 
-configuration.documentation.currentVersion = Version(1, 2, 1)
+configuration.documentation.currentVersion = Version(1, 2, 2)
 
 configuration.documentation.documentationURL = URL(
   string: "https://sdggiesbrecht.github.io/SDGKeyboardDesign"
