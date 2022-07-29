3688a9958d451af4b097d370a3a6572e1e0b1c4e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-07-29 17:47:01 -0600

    ...

--- Sources/SDGKeyboardDesign/Symbols/SetLogic.swift
+++ Sources/SDGKeyboardDesign/Symbols/SetLogic.swift
@@ -21,6 +21,8 @@ extension Symbol {
           return SymbolData(symbol: "∈", names: ["Element"])
         case .françaisFrance:
           return SymbolData(symbol: "∈", names: ["appartient"])
+        case .ελληνικάΕλλάδα:
+          return SymbolData(symbol: "∈", names: ["ανήκει"])
         }
       }),
       LocalizedSymbol({ localization in
@@ -31,6 +33,8 @@ extension Symbol {
           return SymbolData(symbol: "∋", names: ["enthält"])
         case .françaisFrance:
           return SymbolData(symbol: "∋", names: ["contient"])
+        case .ελληνικάΕλλάδα:
+          return SymbolData(symbol: "∋", names: ["περιέχει"])
         }
       }),
       LocalizedSymbol({ localization in
@@ -41,6 +45,8 @@ extension Symbol {
           return SymbolData(symbol: "∩", names: ["Schnittmenge"])
         case .françaisFrance:
           return SymbolData(symbol: "∩", names: ["intersection"])
+        case .ελληνικάΕλλάδα:
+          return SymbolData(symbol: "∩", names: ["τομή"])
         }
       }),
       LocalizedSymbol({ localization in
@@ -51,6 +57,8 @@ extension Symbol {
           return SymbolData(symbol: "∪", names: ["Vereinigungsmenge"])
         case .françaisFrance:
           return SymbolData(symbol: "∪", names: ["union"])
+        case .ελληνικάΕλλάδα:
+          return SymbolData(symbol: "∪", names: ["ένωση"])
         }
       }),
       LocalizedSymbol({ localization in
@@ -61,6 +69,8 @@ extension Symbol {
           return SymbolData(symbol: "∖", names: ["Differenzmenge"])
         case .françaisFrance:
           return SymbolData(symbol: "∖", names: ["complémentaire relatif"])
+        case .ελληνικάΕλλάδα:
+          return SymbolData(symbol: "∖", names: ["σχετικό συμπλήρωμα"])
         }
       }),
       LocalizedSymbol({ localization in
@@ -71,6 +81,8 @@ extension Symbol {
           return SymbolData(symbol: "∆", names: ["symmetrische Differenz"])
         case .françaisFrance:
           return SymbolData(symbol: "∆", names: ["différence symétrique"])
+        case .ελληνικάΕλλάδα:
+          return SymbolData(symbol: "∆", names: ["συμμετρική διαφορά"])
         }
       }),
       LocalizedSymbol({ localization in
@@ -81,6 +93,8 @@ extension Symbol {
           return SymbolData(symbol: "⊆", names: ["Teilmenge"])
         case .françaisFrance:
           return SymbolData(symbol: "⊆", names: ["sous‐ensemble"])
+        case .ελληνικάΕλλάδα:
+          return SymbolData(symbol: "⊆", names: ["υποσύνολο"])
         }
       }),
       LocalizedSymbol({ localization in
@@ -91,6 +105,8 @@ extension Symbol {
           return SymbolData(symbol: "⊇", names: ["Obermenge"])
         case .françaisFrance:
           return SymbolData(symbol: "⊇", names: ["sur‐ensemble"])
+        case .ελληνικάΕλλάδα:
+          return SymbolData(symbol: "⊇", names: ["υπερσύνολο"])
         }
       }),
       LocalizedSymbol({ localization in
@@ -101,6 +117,8 @@ extension Symbol {
           return SymbolData(symbol: "⊊", names: ["echte Teilmenge"])
         case .françaisFrance:
           return SymbolData(symbol: "⊊", names: ["sous‐ensemble propre"])
+        case .françaisFrance:
+          return SymbolData(symbol: "⊊", names: ["γνήσιο υποσύνολο"])
         }
       }),
       LocalizedSymbol({ localization in
@@ -111,6 +129,8 @@ extension Symbol {
           return SymbolData(symbol: "⊋", names: ["echte Obermenge"])
         case .françaisFrance:
           return SymbolData(symbol: "⊋", names: ["sur‐ensemble propre"])
+        case .françaisFrance:
+          return SymbolData(symbol: "⊊", names: ["γνήσιο υπερσύνολο"])
         }
       }),
     ]
