cbb8041f2e731714f5990b84c869559f09eef078
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-10-03 17:05:29 -0600

    Tried Foundation for web.

--- Package.swift
+++ Package.swift
@@ -344,18 +344,18 @@ for target in package.targets {
   var swiftSettings = target.swiftSettings ?? []
   defer { target.swiftSettings = swiftSettings }
   swiftSettings.append(contentsOf: [
-    // #workaround(Swift 5.6.1, Web lacks Foundation.FileMananger.)
-    // #workaround(Swift 5.6.1, Web lacks Foundation.PropertyListSerialization.data(fromPropertyList:format:options:).)
+    // #warning(Swift 5.6.1, Web lacks Foundation.FileMananger.)
+    // #warning(Swift 5.6.1, Web lacks Foundation.PropertyListSerialization.data(fromPropertyList:format:options:).)
     // @example(conditions)
-    .define("PLATFORM_LACKS_FOUNDATION_FILE_MANAGER", .when(platforms: [.wasi])),
-    .define(
+    //.define("PLATFORM_LACKS_FOUNDATION_FILE_MANAGER", .when(platforms: [.wasi])),
+    /*.define(
       "PLATFORM_LACKS_FOUNDATION_PROPERTY_LIST_SERIALIZATION_DATA_FROM_PROPERTY_LIST_FORMAT_OPTIONS",
       .when(platforms: [.wasi])
-    ),
+    ),*/
     // @endExample
 
     // Internal‐only:
-    // #workaround(Swift 5.6.1, Web lacks Foundation.NSHomeDirectory().)
-    .define("PLATFORM_LACKS_FOUNDATION_NS_HOME_DIRECTORY", .when(platforms: [.wasi])),
+    // #warning(Swift 5.6.1, Web lacks Foundation.NSHomeDirectory().)
+    //.define("PLATFORM_LACKS_FOUNDATION_NS_HOME_DIRECTORY", .when(platforms: [.wasi])),
   ])
 }
