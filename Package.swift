e18370affecc0a0045f61815d8b673d552b3d0c5
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-27 00:04:32 -0600

    Restored 5.7 jobs.

--- Package.swift
+++ Package.swift
@@ -359,8 +359,3 @@ for target in package.targets {
     .define("PLATFORM_LACKS_FOUNDATION_NS_HOME_DIRECTORY", .when(platforms: [.wasi])),
   ])
 }
-
-// #workaround(Swift 5.7.2, Hardware compatibility; tools version does not reflect support.))
-#if compiler(<5.8) && !os(macOS)
-  #error("Swift 5.7 is only supported on macOS, tvOS, iOS and watchOS; elsewhere, please use Swift 5.8 or select an older version of SDGKeyboardDesign.")
-#endif
